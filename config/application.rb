b5729604483121e58b45e5d440688af3563c2285
Gordon Eccles <gmeccles@gmail.com>
2016-08-03 08:39:46 -0400

    Setup css precompile since heroku didn't like @import

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Capstone
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.assets.precompile += ['app/assets/stylesheets/signup.scss']
   end
 end
