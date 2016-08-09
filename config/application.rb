7673f5b2c85b3b328ae223e463d7254c173e6f8a
Gordon Eccles <gmeccles@gmail.com>
2016-08-09 12:41:59 -0400

    Rename root directory

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-module Capstone
+module TogetherNote
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
